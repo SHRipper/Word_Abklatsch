9025dc3244475b4596e5b61b801a52b6e516f75c
SHRipper <lukas.schaef@gmx.de>
2015-04-07 17:12:18 +0200

    new

---
+++ Word Abklatsch/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Word_Abklatsch
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Textfeld = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.DateiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NeuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ÖffnenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SpeichernToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SpeichernUnterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SchließenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EinstellungenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontgrößeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Fontgrößen = New System.Windows.Forms.ToolStripComboBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Textfeld
+        '
+        Me.Textfeld.AcceptsReturn = True
+        Me.Textfeld.Location = New System.Drawing.Point(136, 47)
+        Me.Textfeld.Multiline = True
+        Me.Textfeld.Name = "Textfeld"
+        Me.Textfeld.Size = New System.Drawing.Size(465, 246)
+        Me.Textfeld.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DateiToolStripMenuItem, Me.EinstellungenToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(747, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'DateiToolStripMenuItem
+        '
+        Me.DateiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NeuToolStripMenuItem, Me.ÖffnenToolStripMenuItem, Me.SpeichernToolStripMenuItem, Me.SpeichernUnterToolStripMenuItem, Me.ToolStripMenuItem1, Me.SchließenToolStripMenuItem})
+        Me.DateiToolStripMenuItem.Name = "DateiToolStripMenuItem"
+        Me.DateiToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.DateiToolStripMenuItem.Text = "Datei"
+        '
+        'NeuToolStripMenuItem
+        '
+        Me.NeuToolStripMenuItem.Name = "NeuToolStripMenuItem"
+        Me.NeuToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.NeuToolStripMenuItem.Text = "Neu"
+        '
+        'ÖffnenToolStripMenuItem
+        '
+        Me.ÖffnenToolStripMenuItem.Name = "ÖffnenToolStripMenuItem"
+        Me.ÖffnenToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.ÖffnenToolStripMenuItem.Text = "Öffnen"
+        '
+        'SpeichernToolStripMenuItem
+        '
+        Me.SpeichernToolStripMenuItem.Name = "SpeichernToolStripMenuItem"
+        Me.SpeichernToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.SpeichernToolStripMenuItem.Text = "Speichern"
+        '
+        'SpeichernUnterToolStripMenuItem
+        '
+        Me.SpeichernUnterToolStripMenuItem.Name = "SpeichernUnterToolStripMenuItem"
+        Me.SpeichernUnterToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.SpeichernUnterToolStripMenuItem.Text = "Speichern unter"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(154, 6)
+        '
+        'SchließenToolStripMenuItem
+        '
+        Me.SchließenToolStripMenuItem.Name = "SchließenToolStripMenuItem"
+        Me.SchließenToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.SchließenToolStripMenuItem.Text = "Schließen"
+        '
+        'EinstellungenToolStripMenuItem
+        '
+        Me.EinstellungenToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontgrößeToolStripMenuItem})
+        Me.EinstellungenToolStripMenuItem.Name = "EinstellungenToolStripMenuItem"
+        Me.EinstellungenToolStripMenuItem.Size = New System.Drawing.Size(90, 20)
+        Me.EinstellungenToolStripMenuItem.Text = "Einstellungen"
+        '
+        'FontgrößeToolStripMenuItem
+        '
+        Me.FontgrößeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Fontgrößen})
+        Me.FontgrößeToolStripMenuItem.Name = "FontgrößeToolStripMenuItem"
+        Me.FontgrößeToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
+        Me.FontgrößeToolStripMenuItem.Text = "Fontgröße"
+        '
+        'Fontgrößen
+        '
+        Me.Fontgrößen.Name = "Fontgrößen"
+        Me.Fontgrößen.Size = New System.Drawing.Size(121, 23)
+        '
+        'Word_Abklatsch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(747, 468)
+        Me.Controls.Add(Me.Textfeld)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Word_Abklatsch"
+        Me.Text = "Word Abklatsch"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Textfeld As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents DateiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NeuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ÖffnenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SpeichernToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SpeichernUnterToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SchließenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EinstellungenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontgrößeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Fontgrößen As System.Windows.Forms.ToolStripComboBox
+
+End Class
---
+++ Word Abklatsch/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Word_Abklatsch
+
+    'Variablen der Klasse "Word_Abklatsch"
+    Dim savepath As String
+
+    Private Sub Word_Abklatsch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Fontgrößen Combobox füllen
+
+    End Sub
+
+    Private Sub Fontgrößen_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Fontgrößen.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub NeuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NeuToolStripMenuItem.Click
+        Dim NewDR As DialogResult = MessageBox.Show("Willst du wirklich ein neues Dokument erstellen?", "Warnung", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning)
+        If NewDR = Windows.Forms.DialogResult.Yes Then
+            Textfeld.Clear()
+        End If
+    End Sub
+
+    Private Sub ÖffnenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ÖffnenToolStripMenuItem.Click
+        Dim OpenDialog As New OpenFileDialog
+        OpenDialog.InitialDirectory = Environment.SpecialFolder.MyDocuments
+        OpenDialog.Title = "Öffnen..."
+        OpenDialog.Filter = "Textdatei | *.txt" 'Nur Dateien mit Endung ".txt" anzeigen
+        OpenDialog.ShowDialog() 'Dialog anzeigen
+
+        savepath = OpenDialog.FileName 'Speicherpfad ist gleich dem Pfad der geöffneten Datei
+        Textfeld.Text = IO.File.ReadAllText(savepath) 'Text aus Datei in Textfeld einfügen
+    End Sub
+
+    Private Sub SpeichernToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SpeichernToolStripMenuItem.Click
+        If savepath = "" Then
+            Speichern_unter()
+        Else
+            Speichern()
+        End If
+    End Sub
+
+    Private Sub SpeichernUnterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SpeichernUnterToolStripMenuItem.Click
+        Speichern_unter()
+    End Sub
+
+    Private Sub Speichern_unter()
+        Dim SaveAsDialog As New SaveFileDialog
+        SaveAsDialog.InitialDirectory = Environment.SpecialFolder.MyDocuments
+        SaveAsDialog.Title = "Speichern unter..."
+        SaveAsDialog.Filter = "Textdatei | *.txt" 'Nur Textdateien anzeigen
+        SaveAsDialog.ShowDialog() 'Dialog anzeigen
+
+        SaveAsDialog.FileName = savepath 'Pfad der Datei soll nun "savepath" sein
+        IO.File.WriteAllText(savepath, Textfeld.Text) 'Inhalt des Textfeldes in die Datei schreiben
+    End Sub
+
+    Private Sub Speichern()
+        IO.File.WriteAllText(savepath, Textfeld.Text) 'Inhalt des Textfeldes in die Datei schreiben
+    End Sub
+
+    Private Sub SchließenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SchließenToolStripMenuItem.Click
+        Dim CloseDR As DialogResult = MessageBox.Show("Willst du das Programm wirklich schließen?", "Warnung", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning)
+        If CloseDR = Windows.Forms.DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+
+End Class
